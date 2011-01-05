1edaf52dfc5d0d2f940f99027c66fb8249cbe07c
Clément Jobeili <clement.jobeili@gmail.com>
2011-01-05 17:06:44 +0100

    add the event softdelete cascade test

---
+++ test/unit/EventTest.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once dirname(__FILE__).'/../bootstrap/unit.php';
+
+// Database connection
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
+new sfDatabaseManager($configuration);
+
+$event = create_event();
+$event->save();
+
+$date = new Datetime();
+$date1 = new DateTime();
+$date2 = new Datetime();
+
+$date1->sub(date_interval_create_from_date_string('15 days'));
+$date2->add(date_interval_create_from_date_string('15 days'));
+
+$wall1 = create_wall(array(
+	'event_id' 				=> $event->getId(),
+	'name'						=> 'SIP Salle 1',
+	'start'						=> $date1->getTimestamp(),
+	'stop'						=> $date2->getTimestamp(),
+	'real_start_date'	=> $date->getTimestamp()
+));
+
+$wall2 = create_wall(array(
+	'event_id' 				=> $event->getId(),
+	'name'						=> 'SIP Salle 2',
+	'start'						=> $date1->getTimestamp(),
+	'stop'						=> $date2->getTimestamp(),
+	'real_start_date'	=> $date->getTimestamp()
+));
+
+$event->Walls[] = $wall1;
+$event->Walls[] = $wall2;
+
+$event->save();
+$event->delete();
+
+$t = new lime_test(4);
+$t->comment('Softdelete test');
+$t->isnt($event, null, 'Event is not deleted of the database');
+$t->isnt($event->getDeletedAt(), null, 'Event is softdeleted');
+$t->isnt($event->Walls[0]->getDeletedAt(), null, 'Wall1 is soft deleted');
+$t->isnt($event->Walls[1]->getDeletedAt(), null, 'Wall2 is soft deleted');
+
+function create_event($defaults = array())
+{
+	$event = new Event();
+	$event->fromArray(array_merge(array(
+		'name'						=> 'Start In Paris',
+		'short'						=> 'sip',
+		'logo'						=> '12356789.jpg'
+	), $defaults));
+	
+	return $event;
+}
+
+function create_wall($defaults = array())
+{
+	$event = new Wall();
+	$event->fromArray(array_merge(array(
+		'name'						=> 'Start In Paris',
+		'short'						=> 'sip',
+		'logo'						=> '12356789.jpg'
+	), $defaults));
+	
+	return $event;
+}
