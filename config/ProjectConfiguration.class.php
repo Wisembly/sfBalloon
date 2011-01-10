ea0e6b4a4612af08a2fb23ce2d0b38ee91c5e52d
Clément Jobeili <clement.jobeili@gmail.com>
2011-01-10 11:19:51 +0100

    initiate sfDoctrineGuard with extra routes - first draft

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,7 +7,7 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrinePlugin', 'sfDoctrineGuardPlugin');
   }
   
   public function configureDoctrine(Doctrine_Manager $manager) 
