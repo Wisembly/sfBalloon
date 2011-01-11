bbb14eda0eace2f6cddf7bae30ad1ac5778241a0
Clément Jobeili <clement.jobeili@gmail.com>
2011-01-11 11:56:41 +0100

    add event module (work in progress)

---
+++ apps/frontend/modules/event/actions/actions.class.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * event actions.
+ *
+ * @package    balloon
+ * @subpackage event
+ * @author     Clément JOBEILI <clement.jobeili@gmail.com>
+ * @version    SVN: $Id$
+ */
+class eventActions extends sfActions
+{
+  public function preExecute()
+  {
+  }
+  
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->events = $this->getRoute()->getObjects();
+  }
+
+  public function executeNew(sfWebRequest $request)
+  {
+    $this->form = new EventForm();
+  }
+
+  public function executeCreate(sfWebRequest $request)
+  {
+    $this->form = new EventForm();
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('new');
+  }
+
+  public function executeEdit(sfWebRequest $request)
+  {
+    $this->form = new EventForm($this->getRoute()->getObject());
+  }
+
+  public function executeUpdate(sfWebRequest $request)
+  {
+    $this->form = new EventForm($this->getRoute()->getObject());
+
+    $this->processForm($request, $this->form);
+
+    $this->setTemplate('edit');
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->getRoute()->getObject()->delete();
+
+    $this->redirect('event/index');
+  }
+
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+    if ($form->isValid())
+    {
+      $event = $form->save();
+
+      $this->redirect('event/edit?id='.$event->getId());
+    }
+  }
+}
