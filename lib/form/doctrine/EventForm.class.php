4d4f728db312f9fba04a1bb9e5ebf86d68a57dce
Clément Jobeili <clement.jobeili@gmail.com>
2011-01-17 11:52:24 +0100

    add textarea for landing html

--- lib/form/doctrine/EventForm.class.php
+++ lib/form/doctrine/EventForm.class.php
@@ -13,5 +13,7 @@ class EventForm extends BaseEventForm
   public function configure()
   {
     unset($this['updated_at'], $this['created_at'], $this['deleted_at'], $this['redirect'], $this['wall_count']);
+    
+    $this->setWidget('landing_html', new sfWidgetFormTextarea());
   }
 }
