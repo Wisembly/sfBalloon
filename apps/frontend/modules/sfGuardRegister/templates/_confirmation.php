3b045d08b8ae093c52245a81e9120dcf05b9b267
nicophilipp <nicolas.philipp@supinfo.com>
2011-02-22 12:04:32 +0100

    fix erreur dans le code html du mail de confirmation

--- apps/frontend/modules/sfGuardRegister/templates/_confirmation.php
+++ apps/frontend/modules/sfGuardRegister/templates/_confirmation.php
@@ -1,9 +1,11 @@
 <body style="border:0;margin:0;">
-  <div style="background:#4F85BB;width:100%;height:40px;border:0;padding:10px;position:relative;">
-    <img src="http://balloonup.com/images/balloonlogo.png" width="110" alt="Balloon"/>
+  <div style="background:#4F85BB;height:60px;border:0;position:relative;">
+    <div style="margin-top:10px;margin-left:10px;width:110px;float:left;">
+ 		<img src="http://balloonup.com/images/balloonlogo.png" width="110" alt="Balloon"/>
+	</div>
   </div>
-    
-  <div style="float:left;height:100%;">
+
+  <div style="float:left">
     Bienvenue sur votrequestion.com
     <br />
     Cliquer sur <a href="<?php echo url_for('@confirm_mail?token='.$token, array('absolute' => true)) ?>"> pour confirmer votre email</a>
