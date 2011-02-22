8259ec3ae18f50221506fbd786360cee41f7fe7d
nicophilipp <nicolas.philipp@supinfo.com>
2011-02-22 19:02:53 +0100

    add style pour register et login

--- apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,25 +1,32 @@
 <?php use_helper('I18N') ?>
 
 <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-  <table>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
-          
-          <?php $routes = $sf_context->getRouting()->getRoutes() ?>
-          <?php if (isset($routes['sf_guard_forgot_password'])): ?>
+    <div class="sign_table">
+        <div class="col-25">&nbsp;</div>
+        <div class="col-75">
+            <?php foreach ($form as $field): ?>
+                <?php if(!$field->isHidden()):?>
+                    <div class="col-100">
+                        <div id="label"><?php echo $field->renderLabel(); ?></div>
+                        <div id="field"><?php echo $field->render(); ?></div>
+                        <div id="error"><?php echo $field->renderError(); ?></div>
+                    </div>
+                <?php endif ?>
+            <?php endforeach ?>
+        </div>
+        <div class="clear"></div>
+        <div class="tab-center">
+            <input class="login" type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
+            <br/>
+            <?php $routes = $sf_context->getRouting()->getRoutes() ?>
+            <?php if (isset($routes['sf_guard_forgot_password'])): ?>
             <a href="<?php echo url_for('@sf_guard_forgot_password') ?>"><?php echo __('Forgot your password?', null, 'sf_guard') ?></a>
-          <?php endif; ?>
+            <?php endif; ?>
 
-          <?php if (isset($routes['sf_guard_register'])): ?>
+            <?php if (isset($routes['sf_guard_register'])): ?>
             &nbsp; <a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
-          <?php endif; ?>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
+            <?php endif; ?>
+        </div>
+    </div>
 </form>
+<br/>
--- apps/frontend/modules/sfGuardRegister/templates/_form.php
+++ apps/frontend/modules/sfGuardRegister/templates/_form.php
@@ -1,31 +1,25 @@
 <?php use_helper('I18N') ?>
 
 <form action="<?php echo url_for('@sf_guard_register?plan='.$plan) ?>" method="post">
-  <div class="sign_table">
-      <table class="tab-center">
-          <tr>
-              <td>
-      <table>
-        <?php $compteur = 0; ?>
-    <?php foreach ($form as $field): ?>
-        <?php if(strpos($field->render(),'hidden')== false):?>
-            <tr class="field">
-                <th><?php echo $field->renderLabel(); ?></th>
-                <td><?php echo $field->render(); ?></td>
-                <td class="error"><?php echo $field->renderError(); ?><td>
-            </tr>
-            <?php if($compteur == 2) echo'</table></td><td><table>';?>
-            <?php $compteur++;?>
-        <?php endif ?>
-    <?php endforeach ?>
-</table></tr></table>
-  <table class="tab-center">
-      <tr>
-        <td colspan="3" align="center">
-          <input class="register" type="submit" name="register" value="<?php echo __('Register', null, 'sf_guard') ?>" />
-        </td>
-      </tr>
-  </table>
- </div>
+    <div class="sign_table">
+        <div class="col-50">
+            <?php $cp = 0; ?>
+            <?php foreach ($form as $field): ?>
+                <?php if(!$field->isHidden()):?>
+                    <div class="col-100">
+                        <div id="label"><?php echo $field->renderLabel(); ?></div>
+                        <div id="field"><?php echo $field->render(); ?></div>
+                        <div id="error"><?php echo $field->renderError(); ?></div>
+                    </div>
+                    <?php if($cp == 2) echo'</div><div class="col-50">';?>
+                    <?php if($cp == 5) echo'</div><div class="tab-center">';?>
+                    <?php $cp++;?>
+                <?php endif ?>
+            <?php endforeach ?>
+        </div>
+        <div class="tab-center">
+            <input class="register" type="submit" name="register" value="<?php echo __('Register', null, 'sf_guard') ?>" />
+        </div>
+    </div>
 </form>
 <br/>
