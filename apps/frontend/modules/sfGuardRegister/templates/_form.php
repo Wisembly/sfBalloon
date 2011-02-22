ccd42ed43f0befedca2724281ac3fe2c8b336041
nicophilipp <nicolas.philipp@supinfo.com>
2011-02-22 16:56:42 +0100

    skin (html/css) for register

--- apps/frontend/modules/sfGuardRegister/templates/_form.php
+++ apps/frontend/modules/sfGuardRegister/templates/_form.php
@@ -1,14 +1,31 @@
 <?php use_helper('I18N') ?>
 
 <form action="<?php echo url_for('@sf_guard_register?plan='.$plan) ?>" method="post">
-  <table>
-    <?php echo $form ?>
-    <tfoot>
+  <div class="sign_table">
+      <table class="tab-center">
+          <tr>
+              <td>
+      <table>
+        <?php $compteur = 0; ?>
+    <?php foreach ($form as $field): ?>
+        <?php if(strpos($field->render(),'hidden')== false):?>
+            <tr class="field">
+                <th><?php echo $field->renderLabel(); ?></th>
+                <td><?php echo $field->render(); ?></td>
+                <td class="error"><?php echo $field->renderError(); ?><td>
+            </tr>
+            <?php if($compteur == 2) echo'</table></td><td><table>';?>
+            <?php $compteur++;?>
+        <?php endif ?>
+    <?php endforeach ?>
+</table></tr></table>
+  <table class="tab-center">
       <tr>
-        <td colspan="2">
-          <input type="submit" name="register" value="<?php echo __('Register', null, 'sf_guard') ?>" />
+        <td colspan="3" align="center">
+          <input class="register" type="submit" name="register" value="<?php echo __('Register', null, 'sf_guard') ?>" />
         </td>
       </tr>
-    </tfoot>
   </table>
+ </div>
 </form>
+<br/>
--- apps/frontend/modules/sfGuardRegister/templates/indexSuccess.php
+++ apps/frontend/modules/sfGuardRegister/templates/indexSuccess.php
@@ -1,5 +1,7 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('Url'); ?> 
-<h1><?php echo __('Register', null, 'sf_guard') ?></h1>
+<div class="entete">
+    <h1><?php echo __('Register', null, 'sf_guard') ?></h1>
+</div>
 
 <?php echo get_partial('sfGuardRegister/form', array('form' => $form , 'plan' => $plan)) ?>
