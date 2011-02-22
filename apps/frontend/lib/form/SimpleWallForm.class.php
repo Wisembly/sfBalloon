d86a2a2c52978d623c2d74e4aacbb90097170ba4
Clément Jobeili <clement.jobeili@gmail.com>
2011-02-22 14:37:55 +0100

    Add a I18NChoice Language Widget for Wall

--- apps/frontend/lib/form/SimpleWallForm.class.php
+++ apps/frontend/lib/form/SimpleWallForm.class.php
@@ -4,6 +4,11 @@ class SimpleWallForm extends WallForm
 {
   public function configure()
   {
+  	parent::configure();
+
     $this->useFields(array('name','tw_hashtag','sms_hashtag', 'lang', 'is_moderated', 'short_description', 'survey_actived'));
+
+    $this->setWidget('lang', new sfWidgetFormI18nChoiceLanguage());
+    $this->setValidator('lang', new sfValidatorI18nChoiceLanguage());
   }
 }
