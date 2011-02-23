6b53839f6a0678d7841bfc708e1974ce20bfe215
nicophilipp <nicolas.philipp@supinfo.com>
2011-02-23 10:59:52 +0100

    add skin for Home Page (with img/css)

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,1 +1,29 @@
-HOME PAGE
+<div class="center-page">
+    <h1>Bienvenue Sur balloon</h1>
+    <span id="newquotes_container">
+        <p id="m_newaction">votrequestion.com est un service édité par la société Balloon - <a href="http://balloonup.com">http://balloonup.com</a></p>
+    </span>
+</div>
+<div class="left-page">
+    <p>Cet outil vous permet de participer activement à un cours, une conférence, une formation ou un salon. Vous pouvez poser des questions, voir en temps réel les questions posées par la salle, voter pour celles qui vous intéressent... Comment ça marche?</p>
+    <div id="corps">
+        <h2>1) Posez vos questions:</h2>
+        <br/>
+        <div id="clear"></div>
+        <div class="sms"></div>
+        <div class="txt"><strong> SMS</strong>: Envoyer "<em>motclédevotreévénement</em> votre message" au 31 244</div>
+        <div id="clear"></div>
+        <div class="web"></div>
+        <div class="txt"><strong>Web</strong>: Rendez-vous sur l'adresse de l'événement auquel vous assistez et suivez les instructions</div>
+        <div id="clear"></div>
+        <div class="webmobile"></div>
+        <div class="txt"><strong>Webmobile</strong>: Rendez-vous sur l'adresse de l'événement auquel vous assistez depuis votre smartphone et suivez les instructions</div>
+        <div id="clear"></div>
+        <div class="etape-2">
+            <h2>2) Votez et observez les réactions de la salle</h2>
+            <br/>
+            <p>Balloon vous permet de voter  pour les questions qui vous intéressent et de faire remonter les questions les plus populaires</p>
+            <div class="img-center"></div>
+        </div>
+    </div>
+</div>
