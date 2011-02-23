f7a552a2a09c3d3c877f2d850fe1cf905ef0aa12
nicophilipp <nicolas.philipp@supinfo.com>
2011-02-23 15:25:54 +0100

    custom template edit event

--- apps/frontend/modules/wall/templates/editSuccess.php
+++ apps/frontend/modules/wall/templates/editSuccess.php
@@ -1,7 +1,26 @@
-<h3>Editer l'event</h3>
-<p>
+<div class="entete">
+    <h1>Editer l'event</h1>
+</div>
   <form action="<?php echo url_for(sprintf("@wall_edit?event=%s&wall=%s", $eventId, $wallId)); ?>" enctype="multipart/form-data" method="post">
-    <?php echo $form;?>
-    <p><input type="submit" value="Modifier"></p>
+    <div class="sign_table">
+        <div class="col-25">&nbsp;</div>
+        <div class="col-75">
+            <?php foreach ($form as $field): ?>
+                <?php if(!$field->isHidden()):?>
+                    <div class="col-100">
+                        <div id="label"><?php echo $field->renderLabel(); ?></div>
+                        <div id="field"><?php echo $field->render(); ?></div>
+                        <div id="error"><?php echo $field->renderError(); ?></div>
+                    </div>
+                <?php else: ?>
+                    <?php echo $field->render(); ?>
+                <?php endif ?>
+            <?php endforeach ?>
+        </div>
+        <div class="clear"></div>
+        <div class="tab-center">
+            <input class="edit" type="submit" value="Modifier">
+        </div>
+    </div>
   </form>
-</p>
+<br/>
