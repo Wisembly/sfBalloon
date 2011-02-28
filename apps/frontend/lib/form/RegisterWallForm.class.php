7f1d6da876ef2a5977e009cd1f2c183ab035c926
Clément Jobeili <clement.jobeili@gmail.com>
2011-02-28 18:12:17 +0100

    Add a validator, the real start date must be after today

--- apps/frontend/lib/form/RegisterWallForm.class.php
+++ apps/frontend/lib/form/RegisterWallForm.class.php
@@ -6,8 +6,18 @@ class RegisterWallForm extends WallForm
   public function configure() 
   {
     $this->useFields(array('name', 'real_start_date'));
-		
+    
     $this->widgetSchema->setNameFormat('wall[%s]');
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorCallback(array('callback' => array($this, 'checkRealDate')))
+   );
   }
-  
+
+  public function checkRealDate($validator, $values)
+  {
+    $date = new Datetime();
+    if($values['real_start_date'] < $date->format('Y-m-d H:i:s')){
+      throw new sfValidatorError($validator, 'The start date must be after today.');
+    }
+  }
 }
