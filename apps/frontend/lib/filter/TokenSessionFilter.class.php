1d9294a527b5ef9787f370fb71a1c62a13790c34
Clément Jobeili <clement.jobeili@gmail.com>
2011-03-01 09:44:13 +0100

    Fix indent

--- apps/frontend/lib/filter/TokenSessionFilter.class.php
+++ apps/frontend/lib/filter/TokenSessionFilter.class.php
@@ -2,21 +2,20 @@
 
 class TokenSessionFilter extends sfFilter 
 {
-	
-	public function execute($filterChain)
-	{
-		if ($this->isFirstCall()){
-			$user = $this->getContext()->getUser();
-			
-			$ip = $this->getContext()->getRequest()->getRemoteAddress();
-			$info = $this->getContext()->getRequest()->getPathInfoArray();
-			$agent = $info['HTTP_USER_AGENT'];
+  public function execute($filterChain)
+  {
+    if ($this->isFirstCall()){
+      $user = $this->getContext()->getUser();
       
-     	    $token = Tokenisable::generate($ip, $agent);
-			$user->setToken($token, $this->getContext()->getResponse(), $this->getContext()->getRequest());
-		}
-		
-		$filterChain->execute();
-	}
-	
+      $ip = $this->getContext()->getRequest()->getRemoteAddress();
+      $info = $this->getContext()->getRequest()->getPathInfoArray();
+      $agent = $info['HTTP_USER_AGENT'];
+      
+      $token = Tokenisable::generate($ip, $agent);
+      $user->setToken($token, $this->getContext()->getResponse(), $this->getContext()->getRequest());
+    }
+    
+    $filterChain->execute();
+  }
+  
 }
