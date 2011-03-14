60de5949cdbc10a4697a00c0c4215837607d61a0
nicophilipp <nicolas.philipp@supinfo.com>
2011-03-14 18:03:05 +0100

    add css template new wall for an event

--- apps/frontend/modules/event/templates/wallSuccess.php
+++ apps/frontend/modules/event/templates/wallSuccess.php
@@ -1,4 +1,29 @@
+<div id="admin_event_edit">
+    <?php echo link_to('Retour à l\'event '.$event->getShort(),sprintf('@event?short=%s', $event->getShort())); ?>
+</div>
+<div class="entete">
+    <h1>Ajouter un Wall</h1>
+</div>
 <form action="<?php echo url_for(sprintf("@event_add_wall?short=%s", $event->getShort())); ?>" method="post">
-	<?php echo $form; ?>
-	<input type="submit" value="Envoyer">  
+<div class="sign_table">
+    <div class="col-25">&nbsp;</div>
+    <div class="col-75">
+        <?php foreach ($form as $field): ?>
+            <?php if(!$field->isHidden()):?>
+                <div class="col-100">
+                    <div id="label"><?php echo $field->renderLabel(); ?></div>
+                    <div id="field"><?php echo $field->render(); ?></div>
+                    <div id="error"><?php echo $field->renderError(); ?></div>
+                </div>
+            <?php else: ?>
+                <?php echo $field->render(); ?>
+            <?php endif ?>
+        <?php endforeach ?>
+    </div>
+    <div class="clear"></div>
+    <div class="tab-center">
+	<input class="edit" type="submit" value="Envoyer">
+    </div>
+</div>
 </form>
+<br/>
