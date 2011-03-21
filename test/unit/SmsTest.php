ede6c3251993725fe8093f94fe3845b3e04a5375
Clément Jobeili <clement.jobeili@gmail.com>
2011-03-21 11:01:27 +0100

    fix unit test sms

--- test/unit/SmsTest.php
+++ test/unit/SmsTest.php
@@ -11,12 +11,12 @@ $t = new lime_test();
 
 $smsVotes = array(
   array(
-    'from'    => 124567890,
+    'from'    => "124567890".uniqid(),
     'to'      => 235,
     'content' => 'HEC 1'
   ),
   array(
-    'from'    => 1234567890,
+    'from'    => "1234567890".uniqid(),
     'to'      => 235,
     'content' => 'HEC
     1'
@@ -25,12 +25,12 @@ $smsVotes = array(
 
 $smsQuotes = array(
   array(
-    'from'    => 124567890,
+    'from'    => "124567890".uniqid(),
     'to'      => 235,
     'content' => 'HEC Quelle est la couleur du cheval blanc ?'
   ),
   array(
-    'from'    => 1234567890,
+    'from'    => "1234567890".uniqid(),
     'to'      => 235,
     'content' => 'HEC
     Quelle est la couleur du cheval blanc ?'
@@ -40,6 +40,7 @@ $smsQuotes = array(
 
 foreach($smsVotes as $sms){
   $sg = new SMSGateway(new SMSParser(), new SMSDbManager());
+  $sg->setDispatcher(new sfEventDispatcher());
   $resp = $sg->handle($sms);
 
   $t->ok($sg->getSms()->getEventCode() === "HEC", 'The shortcode is OK');
@@ -47,12 +48,13 @@ foreach($smsVotes as $sms){
   $t->ok($sg->getSms()->isVoteToSurvey() === true, 'The shortcode is a vote to a survey');
   $t->ok($sg->getSms()->getContent() === "1", 'The content is valid');
 
-  $t->ok($resp->getStatus() === "ok", 'The response code is ok');
+  $t->ok($resp->getStatus() !== "ok", 'The response code is notok');
 }
 
 
 foreach($smsQuotes as $sms){
   $sg = new SMSGateway(new SMSParser(), new SMSDbManager());
+  $sg->setDispatcher(new sfEventDispatcher());
   $resp = $sg->handle($sms); 
 
   $t->ok($sg->getSms()->getEventCode() === "HEC", 'The shortcode is OK');
